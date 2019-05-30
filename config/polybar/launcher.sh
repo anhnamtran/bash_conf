bfa7ea31ee6a01ac668e4ee49093f19cc9f9db0b
Andrew Tran <andremail03@gmail.com>
2019-05-30 10:15:31 -0700

    Polybar settings

---
+++ config/polybar/launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+pkill polybar
+
+while pgrep -a polybar &> /dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d " " -f 1); do
+    MONITOR=$m polybar --reload mainbar &
+  done
+else
+  polybar --reload mainbar &
+fi
