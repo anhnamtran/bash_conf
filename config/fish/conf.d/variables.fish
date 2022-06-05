c3dfa918b99580d719f165486944725b19f63ad0
Andrew Tran <andremail03@gmail.com>
2022-06-05 13:19:42 -0700

    Move unclutter from xinit to i3

--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -4,7 +4,7 @@ set -gx fish_greeting
 set -gx me (whoami)
 set -gx EDITOR nvim
 
-set -gx VISUAL nvim
+set -gx VISUAL $EDITOR
 set -gx GIT_DISCOVERY_ACROSS_FILESYSTEM 1
 
 set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
