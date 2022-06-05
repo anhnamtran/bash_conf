3c2ec21011f5983802b6b6e5a541c85b72974eb4
Andrew Tran <andremail03@gmail.com>
2022-06-05 12:09:15 -0700

    Add touchpad module

--- bin/toggle_touchpad.sh
+++ bin/toggle_touchpad.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 TOUCHPAD_OFF=$(synclient -l | grep Touchpad | awk '{ print $3 }')
 
+if [[ "$1" == "status" ]]; then
+  echo "$TOUCHPAD_OFF"
+  exit "$TOUCHPAD_OFF"
+fi
+
 if (( TOUCHPAD_OFF == 0 )); then
   synclient Touchpadoff=1
 else
