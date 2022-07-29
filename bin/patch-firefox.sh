92efa47f13fd6466363b7e21e56473afda7491ea
Andrew Tran <andremail03@gmail.com>
2022-07-28 17:02:36 -0700

    [Cron] Backup of dotfiles

--- bin/patch-firefox.sh
+++ bin/patch-firefox.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if ! sudo grep 'reserved="true"' /usr/lib/firefox/browser/omni.ja; then
+  echo "Firefox already patched"
+  exit 0
+fi
 sudo perl -i -pne 's/reserved="true"/               /g' /usr/lib/firefox/browser/omni.ja
 find ~/.cache/mozilla/firefox -type d -name startupCache -print0 | xargs -0 rm -rf
