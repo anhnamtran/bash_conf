5cc30fa65e75ee8a46b8455de0d951035458f1a3
Andrew Tran <andremail03@gmail.com>
2022-09-19 09:57:22 -0700

    Fixup firefox patcher

--- bin/patch-firefox.sh
+++ bin/patch-firefox.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "Usage: sudo ./patch-firefox.sh <USER>"
+  exit 1
+fi
+USER=$1
 if ! sudo grep 'reserved="true"' /usr/lib/firefox/browser/omni.ja; then
   echo "Firefox already patched"
-  exit 0
+else
+  sudo perl -i -pne 's/reserved="true"/               /g' /usr/lib/firefox/browser/omni.ja
 fi
-sudo perl -i -pne 's/reserved="true"/               /g' /usr/lib/firefox/browser/omni.ja
-find ~/.cache/mozilla/firefox -type d -name startupCache -print0 | xargs -0 rm -rf
+echo "Clearing cache"
+find /home/$USER/.cache/mozilla/firefox -type d -name startupCache -print0 | xargs -0 rm -rf
