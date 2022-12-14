3db9003ad2cb9bcd22b139a0bcc1ff39d59ae218
Andrew Tran <andremail03@gmail.com>
2022-12-14 10:21:07 -0800

    config/nvim: lua-fy startify

--- config/nvim/lua/andrew_nt/startify.lua
+++ config/nvim/lua/andrew_nt/startify.lua
@@ -1,26 +1,20 @@
 -- Setup for startify
-vim.cmd([[
-let g:startify_session_dir = g:sessions_dir
-let g:startify_ascii_header = [
-      \ ' █████╗  ███╗   ██╗ ██████╗  ██████╗  ███████╗ ██╗    ██╗',
-      \ '██╔══██╗ ████╗  ██║ ██╔══██╗ ██╔══██╗ ██╔════╝ ██║    ██║',
-      \ '███████║ ██╔██╗ ██║ ██║  ██║ ██████╔╝ █████╗   ██║ █╗ ██║',
-      \ '██╔══██║ ██║╚██╗██║ ██║  ██║ ██╔══██╗ ██╔══╝   ██║███╗██║',
-      \ '██║  ██║ ██║ ╚████║ ██████╔╝ ██║  ██║ ███████╗ ╚███╔███╔╝',
-      \ '╚═╝  ╚═╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═╝  ╚═╝ ╚══════╝  ╚══╝╚══╝',
-      \]
-let g:startify_custom_header = startify#pad(g:startify_ascii_header)
-let g:startify_lists = [
-      \ { 'type': 'commands',  'header': ['   Commands']        },
-      \ { 'type': 'sessions',  'header': ['   Sessions']        },
-      \ { 'type': 'files',     'header': ['   FILES']           },
-      \ { 'type': 'dir',       'header': ['   DIRS '. getcwd()] },
-      \ ]
-let g:startify_commands = [
-      \ {'v': ['vimrc', 'e ~/.vimrc']},
-      \ {'P': ['Update plugins', 'PlugUpdate']},
-      \ {'w': ['VimWiki Index', 'VimwikiIndex']},
-      \ {'d': ['VimWiki Diary', 'VimwikiDiary']},
-      \ {'D': ['VimWiki Diary Index', 'VimwikiDiaryIndex']},
-      \ ]
-]])
+vim.g.startify_session_dir = vim.g.sessions_dir
+vim.g.startify_ascii_header = {
+' █████╗ ███╗   ██╗██████╗ ██████╗ ███████╗██╗    ██╗',
+'██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔════╝██║    ██║',
+'███████║██╔██╗ ██║██║  ██║██████╔╝█████╗  ██║ █╗ ██║',
+'██╔══██║██║╚██╗██║██║  ██║██╔══██╗██╔══╝  ██║███╗██║',
+'██║  ██║██║ ╚████║██████╔╝██║  ██║███████╗╚███╔███╔╝',
+'╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝',
+}
+vim.g.startify_custom_header =
+  vim.fn['startify#pad'](vim.g.startify_ascii_header)
+vim.g.startify_lists = {
+  { type = 'commands', header = { '    Commands' } },
+  { type = 'sessions', header = { '    Sessions' } },
+}
+vim.g.startify_commands = {
+  { i = { 'init.lua', 'e ~/.config/nvim/init.lua' } },
+  { P = { 'Update plugins', 'PackerCompile <BAR> PackerSync' } },
+}
