d49e5d2c4746a3a40200f11f17bfedc4ff8c4ed7
Andrew Tran <andremail03@gmail.com>
2022-12-22 12:40:02 -0800

    config/nvim: install nvim-osc52 plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -112,6 +112,7 @@ return require('packer').startup(function(use)
   use 'easymotion/vim-easymotion'
   use 'karb94/neoscroll.nvim'
   use 'lewis6991/impatient.nvim'
+  use 'ojroques/nvim-osc52'
 
   -- Arista specific plugins
   use 'https://gitlab.aristanetworks.com/vim-scripts/mts.vim'
