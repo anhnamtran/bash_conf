e69222991772a7685e5ea0f458be79133b8ce873
Andrew Tran <andremail03@gmail.com>
2023-01-15 19:30:48 -0800

    config/polybar: add a secondary bar with less modules

--- config/polybar/launcher.sh
+++ config/polybar/launcher.sh
@@ -12,8 +12,12 @@ if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d " " -f 1); do
     if [ "$m" = "$PRIMARY_MONITOR" ]; then
       TRAY_POS='right'
+      BAR=mainbar
+    else
+      TRAY_POS='none'
+      BAR=secondarybar
     fi
-    TRAY_POS=${TRAY_POS:-none} MONITOR=$m polybar --reload mainbar < /dev/null > /tmp/polybar.$m.log 2>&1 200>&- & disown
+     TRAY_POS=$TRAY_POS MONITOR=$m polybar --reload $BAR < /dev/null > /tmp/polybar.$m.log 2>&1 200>&- & disown
     unset TRAY_POS
   done
 else
