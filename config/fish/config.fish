66ffdcae90b137dcc64a269ed3fbc3eb8a63ed5d
Andrew Tran <andremail03@gmail.com>
2023-01-17 12:46:49 -0800

    Install anaconda3

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,9 @@ end
 function fish_vi_cursor; end
 export PATH="$PATH:$HOME/.spicetify"
 fish_add_path /home/andrew_nt/.spicetify
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/andrew_nt/code/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
