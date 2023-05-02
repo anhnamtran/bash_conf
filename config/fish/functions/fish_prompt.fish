c0982fb5295db1c70c70fcff3ae8616f61922098
Andrew Tran <andremail03@gmail.com>
2023-05-02 16:09:21 -0700

    config/fish: fixup git symbols

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -24,9 +24,9 @@ function fish_prompt
 
    set -g __fish_git_prompt_char_stateseparator " 〉"
    set -g __fish_git_prompt_char_stagedstate " "
-   set -g __fish_git_prompt_char_dirtystate "󰐕 "
+   set -g __fish_git_prompt_char_dirtystate "󰐕"
    set -g __fish_git_prompt_char_untrackedfiles " "
-   set -g __fish_git_prompt_char_conflictedstate " "
+   set -g __fish_git_prompt_char_conflictedstate ""
    set -g __fish_git_prompt_char_cleanstate " "
    set -g __fish_git_prompt_char_stashstate "󰆢 "
 
