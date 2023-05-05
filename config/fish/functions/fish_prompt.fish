84ee96196a9bb751817c2a68415c701d9f0914a6
Andrew Tran <andremail03@gmail.com>
2023-05-05 12:57:11 -0700

    config/fish: symbol updates

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -19,16 +19,16 @@ function fish_prompt
 
    set -g __fish_git_prompt_color_branch yellow
    set -g __fish_git_prompt_showupstream "informative"
-   set -g __fish_git_prompt_char_upstream_ahead ""
-   set -g __fish_git_prompt_char_upstream_behind ""
+   set -g __fish_git_prompt_char_upstream_ahead " "
+   set -g __fish_git_prompt_char_upstream_behind " "
    set -g __fish_git_prompt_char_upstream_prefix ""
 
    set -g __fish_git_prompt_char_stateseparator " 〉"
-   set -g __fish_git_prompt_char_stagedstate ""
-   set -g __fish_git_prompt_char_dirtystate "󰐕"
-   set -g __fish_git_prompt_char_untrackedfiles ""
-   set -g __fish_git_prompt_char_conflictedstate ""
-   set -g __fish_git_prompt_char_cleanstate ""
+   set -g __fish_git_prompt_char_stagedstate " "
+   set -g __fish_git_prompt_char_dirtystate "󰐕 "
+   set -g __fish_git_prompt_char_untrackedfiles " "
+   set -g __fish_git_prompt_char_conflictedstate " "
+   set -g __fish_git_prompt_char_cleanstate " "
    set -g __fish_git_prompt_char_stashstate "󰆢 "
 
    set -g __fish_git_prompt_color_dirtystate blue
