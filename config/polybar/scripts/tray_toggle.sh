8df88b83c0118bcc30d04206ce9a6995265e1ed8
Andrew Tran <andremail03@gmail.com>
2023-08-01 10:17:15 -0700

    config/polybar: auto-hide tray

--- config/polybar/scripts/tray_toggle.sh
+++ config/polybar/scripts/tray_toggle.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Script parameters are passed as environment variables for ease of use with
+# built-in tray module
+AUTO_HIDE_TRAY=${AUTO_HIDE_TRAY:-false}
+START_TRAY_HIDDEN=${START_TRAY_HIDDEN:-false}
+
 CACHE_DIR="$HOME/.cache/polybar-scripts"
 mkdir -p "$CACHE_DIR"
 
@@ -6,35 +11,68 @@ mkdir -p "$CACHE_DIR"
 LOCK_FILE="tray_toggle.lock"
 
 SLEEP_PID=0
+SELF_PID=$$
+
+VISIBILITY_TIMEOUT=5
+AUTO_HIDE_PID=0
+
+DEBUG=${DEBUG:-}
+debug() {
+  if [[ -z "$DEBUG" ]]; then
+    return
+  fi
+  echo "DEBUG: $*"
+}
+
+auto_hide() {
+  if [[ "$AUTO_HIDE_TRAY" = "false" ]]; then
+    return
+  fi
+  debug "Waiting $VISIBILITY_TIMEOUT before hiding"
+  sleep $VISIBILITY_TIMEOUT &
+  AUTO_HIDE_PID=$!
+  wait
+
+  if [[ -f "$CACHE_DIR/$LOCK_FILE" ]]; then
+    debug "Tray already hidden"
+    return
+  fi
+  kill -USR1 $SELF_PID
+}
 
 show_tray() {
+  debug "Showing tray"
+
   polybar-msg action tray module_show &>/dev/null
   echo ""
+
+  rm -f "$CACHE_DIR/$LOCK_FILE"
+
+  auto_hide
 }
 
 hide_tray() {
+  debug "Hiding tray"
+
   polybar-msg action tray module_hide &>/dev/null
   echo "󰄽"
+
+  touch "$CACHE_DIR/$LOCK_FILE"
 }
 
 handle_click() {
   if [ -f "$CACHE_DIR/$LOCK_FILE" ]; then
     show_tray
-    rm -f "$CACHE_DIR/$LOCK_FILE"
   else
     hide_tray
-    touch "$CACHE_DIR/$LOCK_FILE"
   fi
 }
 
 handle_start() {
   if [ "$START_TRAY_HIDDEN" = "true" ]; then
     hide_tray
-    # Workaround for the tray overallping on startup
-    touch "$CACHE_DIR/$LOCK_FILE"
   else
     show_tray
-    rm -f "$CACHE_DIR/$LOCK_FILE"
   fi
 }
 
@@ -42,12 +80,15 @@ handle_sig() {
   if (( $SLEEP_PID != 0 )); then
     kill $SLEEP_PID &>/dev/null
   fi
+  if (( $AUTO_HIDE_PID != 0 )); then
+    kill $AUTO_HIDE_PID &>/dev/null
+  fi
   handle_click
 }
 
-handle_start
-
 trap "handle_sig" SIGUSR1
+
+handle_start
 while true; do
   sleep infinity &
   SLEEP_PID=$!
