8308156316b82684c7e01b1ae343aebc3a51ce03
Andrew Tran <andremail03@gmail.com>
2023-11-14 17:06:52 -0800

    [andrew_nt@DahliaEndeavor] Automatic backup of tracked files

--- config/nvim/lua/andrew_nt/coc.lua
+++ config/nvim/lua/andrew_nt/coc.lua
@@ -104,7 +104,7 @@ local mappings = {
         {'<C-F>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "<Right>"', { expr = true, silent = true, nowait = true }},
         {'<C-B>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "<Left>"', { expr = true, silent = true, nowait = true }},
         -- Make <CR> either confirm completion or call nvim-autopairs enter
-        {'<CR>',  'coc#pum#visible() && coc#pum#info()["index"] != -1 ? coc#pum#confirm() : v:lua.require("nvim-autopairs").autopairs_cr()', {expr = true, noremap = true}},
+        {'<CR>',  'coc#pum#visible() && coc#pum#info()["index"] != -1 ? coc#pum#confirm() : v:lua.require\'nvim-autopairs\'.autopairs_cr()', {expr = true, noremap = true}},
 	},
 	n = { -- Normal mode
         {"K", '<CMD>lua _G.show_docs()<CR>', { silent = true }},
