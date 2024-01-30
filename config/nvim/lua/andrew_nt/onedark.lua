74d59a1d0335259d972b88dfbf79d30763201c47
Andrew Tran <andremail03@gmail.com>
2024-01-29 19:08:01 -0800

    config/nvim: update onedark markdown color

--- config/nvim/lua/andrew_nt/onedark.lua
+++ config/nvim/lua/andrew_nt/onedark.lua
@@ -5,14 +5,14 @@ require('onedark').setup({
       ['@statement'] = { fg = '$purple', fmt = 'bold' },
       ['@storageclass'] = { fg = '$purple' },
       ['@structure'] = { fg = '$yellow' },
-      ['@markup.heading.1'] = { fg = '$orange', fmt = 'bold' },
-      ['@markup.heading.2'] = { fg = '$green', fmt = 'bold' },
-      ['@markup.heading.3'] = { fg = '$cyan', fmt = 'bold' },
-      ['@markup.heading.4'] = { fg = '$yellow', fmt = 'bold' },
-      ['@markup.heading.1.marker'] = { fg = '$orange', fmt = 'bold' },
-      ['@markup.heading.2.marker'] = { fg = '$green', fmt = 'bold' },
-      ['@markup.heading.3.marker'] = { fg = '$cyan', fmt = 'bold' },
-      ['@markup.heading.4.marker'] = { fg = '$yellow', fmt = 'bold' },
+      ['@markup.heading.1.markdown'] = { fg = '$orange', fmt = 'bold' },
+      ['@markup.heading.2.markdown'] = { fg = '$green', fmt = 'bold' },
+      ['@markup.heading.3.markdown'] = { fg = '$cyan', fmt = 'bold' },
+      ['@markup.heading.4.markdown'] = { fg = '$yellow', fmt = 'bold' },
+      ['@markup.heading.1.marker.markdown'] = { fg = '$orange', fmt = 'bold' },
+      ['@markup.heading.2.marker.markdown'] = { fg = '$green', fmt = 'bold' },
+      ['@markup.heading.3.marker.markdown'] = { fg = '$cyan', fmt = 'bold' },
+      ['@markup.heading.4.marker.markdown'] = { fg = '$yellow', fmt = 'bold' },
    }
 })
 require('onedark').load()
