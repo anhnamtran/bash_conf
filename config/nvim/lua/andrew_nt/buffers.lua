f946a9e8ab04123a33fa522f2d7fc70029ff7930
Andrew Tran <andremail03@gmail.com>
2024-02-20 16:06:48 -0800

    config/nvim: fixup buffer detection in other tabs for buffer wipeout

--- config/nvim/lua/andrew_nt/buffers.lua
+++ config/nvim/lua/andrew_nt/buffers.lua
@@ -11,7 +11,7 @@ nmap('H', ':bp<CR>', true)
 nmap('L', ':bn<CR>', true)
 -- Use vim-bbye if possible
 if require("lazy.core.config").plugins["vim-bbye"] ~= nil then
-   nmap('<leader>q', ':Bwipeout #<CR>', true)
+   nmap('<leader>q', ':Bwipeout<CR>', true)
 else
    nmap('<leader>q', ':bp <BAR> bwipeout #<CR>', true)
 end
@@ -22,11 +22,11 @@ local function DeleteInactiveBufs()
   local visiblebufs = {}
   for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
      local bufs = vim.fn.tabpagebuflist(tab)
-     visiblebufs = vim.tbl_extend("keep", visiblebufs, bufs)
+     visiblebufs = vim.list_extend(visiblebufs, bufs)
   end
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     -- The buffer is valid, is not modified, and exists in the buffer list
-    if vim.api.nvim_buf_is_valid(buf) and not vim.api.nvim_buf_get_option(buf, "modified") and not vim.tbl_contains(visiblebufs, buf) then
+    if vim.api.nvim_buf_is_loaded(buf) and not vim.api.nvim_buf_get_option(buf, "modified") and not vim.tbl_contains(visiblebufs, buf) then
       if require("lazy.core.config").plugins["vim-bbye"] ~= nil then
         vim.cmd["Bwipeout"](buf)
       else
@@ -38,4 +38,4 @@ local function DeleteInactiveBufs()
   vim.notify(nbufs .. " buffer(s) wiped out", vim.log.levels.INFO)
 end
 vim.api.nvim_create_user_command("OpenOnly", DeleteInactiveBufs, {})
-nmap("<C-w>b", ":OpenOnly")
+nmap("<C-w>b", ":OpenOnly<CR>")
