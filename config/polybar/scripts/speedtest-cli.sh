a30ab69c72ec9ebd7af94d1f4794835f20afbb6e
Andrew Tran <andremail03@gmail.com>
2024-04-24 13:19:12 -0700

    config/polybar: fix up netspeed module

--- config/polybar/scripts/speedtest-cli.sh
+++ config/polybar/scripts/speedtest-cli.sh
@@ -5,21 +5,24 @@ fi
 
 SLEEP_PID=0
 SLEEP_INT="$1"
+SHOULD_SLEEP=1
+
+TEST_PID=0
+TEST_OUT="$HOME/.cache/polybar-scripts/speedtest-cli.json"
 
 run_speedtest() {
-  if ! nc -zw1 google.com 443; then
+  SHOULD_SLEEP=1
+  if ! nc -zw1 google.com 443 &>/dev/null; then
     echo "No connection."
     exit 1
   fi
 
-  if (( $SLEEP_PID != 0 )); then
-    kill $SLEEP_PID &>/dev/null
-  fi
-
   echo "Running speedtest..."
-  local speed_test_json=$(speedtest-cli --secure --timeout 5 --json 2> /dev/null)
-  local download=$(echo "$speed_test_json" | jq '.download' | numfmt --to iec --format '%.2fb/s')
-  local upload=$(echo "$speed_test_json" | jq '.upload' | numfmt --to iec --format '%.2fb/s')
+  speedtest-cli --secure --timeout 5 --json 2> /dev/null > "$TEST_OUT" &
+  TEST_PID=$!
+  wait
+  local download=$(cat "$TEST_OUT" | jq '.download' | numfmt --to iec --format '%.2fb/s')
+  local upload=$(cat "$TEST_OUT" | jq '.upload' | numfmt --to iec --format '%.2fb/s')
   if [[ -z "$download" && -z "$upload" ]]; then
     echo ""
     return
@@ -27,11 +30,23 @@ run_speedtest() {
   echo " $download" " $upload"
 }
 
-trap run_speedtest SIGUSR1
+handle_signal() {
+  if (( $SLEEP_PID != 0 )); then
+    kill "$SLEEP_PID" &>/dev/null
+  fi
+  if (( $TEST_PID != 0 )); then
+    kill "$TEST_PID" &>/dev/null
+    SHOULD_SLEEP=0
+  fi
+}
+
+trap handle_signal SIGUSR1
 
 while true; do
   run_speedtest
-  sleep "$SLEEP_INT" &
-  SLEEP_PID=$!
+  if (( SHOULD_SLEEP != 0 )); then
+    sleep "$SLEEP_INT" &
+    SLEEP_PID=$!
+  fi
   wait
 done
