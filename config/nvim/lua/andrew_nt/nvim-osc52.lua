9d359b4dc1edaeb2e1d8d67a6e750aebd197c82d
Andrew Tran <andremail03@gmail.com>
2024-05-27 17:00:01 -0700

    [Cron] Backup of dotfiles

--- config/nvim/lua/andrew_nt/nvim-osc52.lua
+++ config/nvim/lua/andrew_nt/nvim-osc52.lua
@@ -1,18 +1,18 @@
 -- Setup for nvim-osc52
--- require('osc52').setup {
---   max_length = 10000,
---   silent = false,
---   trim = true,
--- }
--- Force vim to use OSC52 to copy and paste certain registers
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
+require('osc52').setup {
+  max_length = 10000,
+  silent = false,
+  trim = true,
 }
+-- Force vim to use OSC52 to copy and paste certain registers
+-- vim.g.clipboard = {
+--   name = 'OSC 52',
+--   copy = {
+--     ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+--     ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+--   },
+--   paste = {
+--     ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+--     ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+--   },
+-- }
