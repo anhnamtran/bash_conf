16a79e0dcef75b302c22a443a0952b4c18aaf8c7
Andrew Tran <andremail03@gmail.com>
2026-02-17 13:51:22 -0800

    config/nvim: sync flash from dev

--- config/nvim/lua/andrew_nt/flash.lua
+++ config/nvim/lua/andrew_nt/flash.lua
@@ -1,9 +1,6 @@
 -- Settings for flash.nvim
 require('flash').setup({
   keys = 'abcdefghijklmnop',
-  search = {
-    incremental = true,
-  },
   modes = {
     char = {
        enabled = false,
@@ -11,8 +8,27 @@ require('flash').setup({
   },
 })
 
+function flash_word()
+  require("flash").jump({
+    pattern = ".", -- initialize pattern with any char
+    search = {
+      mode = function(pattern)
+         return "\\<" .. pattern
+      end,
+    },
+    -- select the range
+    jump = { pos = "start" },
+  })
+end
+
 vim.keymap.set(
   'n',
   '<c-space>',
   function() require('flash').jump() end,
+  { noremap = true, silent = true })
+
+vim.keymap.set(
+  'n',
+  '<leader>w',
+  flash_word,
   { noremap = true, silent = true })
