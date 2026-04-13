0b9441afa2211f3e08da527666b5c6be4fb15bff
Andrew Tran <andremail03@gmail.com>
2026-04-12 17:00:00 -0700

    [andrew_nt@DahliaEndeavor] Automatic backup of tracked files

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -2,3 +2,4 @@ fish_add_path -m /usr/local/bin
 fish_add_path -a /home/andrew_nt/bin
 fish_add_path -am /home/andrew_nt/.local/bin
 fish_add_path -am /home/andrew_nt/.cargo/bin
+fish_add_path -am /home/andrew_nt/.npm-global/bin
