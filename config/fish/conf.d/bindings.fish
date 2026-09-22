5dfe7aa59d0503a3fe0c0823c0783ac6ec8510ec
Andrew Tran <andremail03@gmail.com>
2026-09-22 10:23:28 -0700

    config/fish: disable jj binding

--- config/fish/conf.d/bindings.fish
+++ config/fish/conf.d/bindings.fish
@@ -1,7 +1,8 @@
 # Change to Vi mode
 function fish_user_key_bindings
    fish_vi_key_bindings insert
-   bind -M insert -m default jj 'backward-char' 'repaint-mode'
+   # disabled for "jj-vcs" usage
+   # bind -M insert -m default jj 'backward-char' 'repaint-mode'
    bind -M insert qq 'exit'
    bind -M default k 'up-or-search'
    bind -M default j 'down-or-search'
